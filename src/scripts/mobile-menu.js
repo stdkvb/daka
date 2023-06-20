2d987e0199cef82d630ddc4d39aef0ba158c175d
Bulat Sitdikov <rdvad2@gmail.com>
2023-06-20 17:36:21 +0500

    add product pages

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -3,15 +3,18 @@ const menuController = function () {
 	const menu = document.getElementById('menu')
 	burger.addEventListener('click', () => {
 		menu.classList.toggle('active')
+		document.body.style.overflow = 'hidden'
 	})
 	const close = document.getElementById('close')
 	close.addEventListener('click', () => {
 		menu.classList.toggle('active')
+		document.body.style.overflow = 'unset'
 	})
 	const links = menu.querySelectorAll('a')
 	links.forEach((element) => {
 		element.addEventListener('click', () => {
 			menu.classList.toggle('active')
+			document.body.style.overflow = 'unset'
 		})
 	});
 }
